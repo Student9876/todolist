1e6fc9d692e30d51167dc7850808addc9fa6d36e
Student9876 <shouvik9876@gmail.com>
2023-05-13 08:37:13 +0530

    Port env edit in app.js

--- app.js
+++ app.js
@@ -140,6 +140,6 @@ app.post("/delete", (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server is running on port " + process.env.PORT)
 })
