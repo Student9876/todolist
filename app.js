79ead85d301d47cd8df9ed455cba472b06a15b72
Student9876 <shouvik9876@gmail.com>
2023-05-13 19:17:43 +0530

    Mobile view fixed

--- app.js
+++ app.js
@@ -140,6 +140,6 @@ app.post("/delete", (req, res) => {
 })
 
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT, function () {
     console.log("Server is running on port " + process.env.PORT)
 })
